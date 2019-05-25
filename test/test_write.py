8a8bb9cd33f4c99e8f71492c494890bc64d48042
leotrs <leo@leotrs.com>
2019-05-25 15:54:42 -0400

    separate things into decoders and encoders

--- test/test_write.py
+++ test/test_write.py
@@ -17,7 +17,7 @@ def test_edgelist():
     graph.edges = [((0, 1), 1), ((1, 2), 2), ((2, 0), 1)]
 
     with_data = 'a b 1\nb c 2\nc a 1\n'
-    assert with_data == graph.write_edgelist(None, return_text=True, data=True)
+    assert with_data == graph.decode('edgelist', None, return_text=True, data=True)
 
     no_data = 'a b\nb c\nc a\n'
-    assert no_data == graph.write_edgelist(None, return_text=True, data=False)
+    assert no_data == graph.decode('edgelist', None, return_text=True, data=False)
