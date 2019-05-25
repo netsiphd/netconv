159ab7e5a2215669141ab23f7ea296966e511fc8
leotrs <leo@leotrs.com>
2019-05-25 15:09:48 -0400

    minimal setup for installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='netconv',
+      version='0.0.1',
+      description='Convert between different graph formats',
+      author='NetSci\'19 Open Hackathon',
+      author_email='leo@leotrs.com',
+      packages=[],
+     )
