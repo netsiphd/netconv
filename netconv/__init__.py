572efefc001c9415a47f66551291df0541783783
Jean-Gabriel Young <jean.gabriel.young@gmail.com>
2019-05-25 16:10:20 -0400

    Autho info and barebone CLI

--- netconv/__init__.py
+++ netconv/__init__.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+netconv is a lightweight graph format converter.
+
+Authors:  Chia-Hung Yang <yang.chi@husky.neu.edu>
+          Leonardo Torres <leo@leotrs.com>
+          Stefan McCabe <mccabe.s@husky.neu.edu>
+          Jean-Gabriel Young <jgyou@umich.edu>
+"""
 from .graph import GraphData
 
+
 __all__ = ['GraphData']
+
+
+__authors__ = ["Chia-Hung Yang <yang.chi@husky.neu.edu>",
+               "Leonardo Torres <leo@leotrs.com>",
+               "Stefan McCabe <mccabe.s@husky.neu.edu>",
+               "Jean-Gabriel Young <jgyou@umich.edu>"]
+__license__ = "MIT"
+__name__ = 'netconv'
+__date__ = "May 26th, 2019"
+__version__ = '0.0.1'
+__contact__ = 'leo@leotrs.com'
+__desc__ = 'Lightweight graph format converter.'
