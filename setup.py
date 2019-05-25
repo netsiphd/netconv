4bfa2943dbf8289f5462abf1e08e7443e8477472
YY Ahn <yongyeol@gmail.com>
2019-05-25 16:12:17 -0400

    making setup.py to find the all available packages

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
+from setuptools import find_packages
 
-setup(name='netconv',
-      version='0.0.1',
-      description='Convert between different graph formats',
-      author='NetSci\'19 Open Hackathon',
-      author_email='leo@leotrs.com',
-      packages=[],
-     )
+setup(
+    name="netconv",
+    version="0.0.1",
+    description="Convert between different graph formats",
+    author="NetSci'19 Open Hackathon",
+    author_email="leo@leotrs.com",
+    packages=find_packages(),
+)
